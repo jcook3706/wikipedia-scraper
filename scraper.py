96152320394241e50a389673d6bac815f9e6d28d
jcook3706 <jcook3706@gmail.com>
2022-12-18 15:59:13 -0500

    working scraper, added depth and number of pages limits

--- scraper.py
+++ scraper.py
@@ -1,12 +1,21 @@
 import requests
 import time
+import json
 from bs4 import BeautifulSoup
 
+lastTime= time.time()
 
 def getLinks(urlEndpoint, links):
+    global lastTime
     links[urlEndpoint] = []
-    response = requests.get('https://en.wikipedia.org?', urlEndpoint)
+    timeDelta = time.time()-lastTime
+    #print(f'Time spent computing: {timeDelta}')
+    lastTime = time.time()
+    response = requests.get(f'https://en.wikipedia.org{urlEndpoint}')
     soup = BeautifulSoup(response.text, 'html.parser')
+    timeDelta = time.time()-lastTime
+    lastTime = time.time()
+    # print(f'Time spent waiting for response: {timeDelta}')
     for link in soup.find_all('a'):
         linkStr = str(link.get('href'))
         if(linkStr.find(':') != -1):
@@ -24,9 +33,27 @@ def validLink(linkStr):
     return True
 
 def main():
+    startTime = time.time()
+    numToScrape = 1000
+    depthToScrape = 2
     links = {}
+    visited = []
+    count = 0
     getLinks('/wiki/Web_scraping', links)
-    print(links)
-
+    # print(links)
+    for i in range(depthToScrape):
+        for key in list(links.keys()):
+            for link in links[key]:
+                if link not in visited:
+                    getLinks(link, links)
+                    visited.append(link)
+                count+=1
+                if(count%10 == 0):
+                    print(count, ' wiki pages scraped')
+                #time.sleep(2)
+                if(count>numToScrape):
+                    print(links.keys())
+                    print(f'Average time per page: {(time.time()-startTime)/numToScrape}')
+                    return
 if __name__ == '__main__':
     main()
