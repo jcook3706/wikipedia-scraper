b78213fe47ac6c7836a8747a6aafc474c9150696
jcook3706 <jcook3706@gmail.com>
2022-12-18 20:38:23 -0500

    first (maybe) working multithreaded version

--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 from os.path import exists
 
 lastTime= time.time()
+numThreads = 4
 
 def getLinks(urlEndpoint, links):
     global lastTime
@@ -37,7 +38,7 @@ def validLink(linkStr):
 
 def main():
     startTime = time.time()
-    numToScrape = 10
+    numToScrape = 100
     depthToScrape = 2
     if not exists('links.txt'):
         links = {}
@@ -63,16 +64,56 @@ def bfsIterate(numToScrape, depthToScrape, links, visited):
     count = 0
     for i in range(depthToScrape):
         for key in list(links.keys()):
-            for link in links[key]:
-                if link not in visited:
-                    getLinks(link, links)
-                    visited.append(link)
-                    count+=1
+            for i in range(0, len(links[key]), numThreads):
+                if len(links[key])-i > numThreads and count+numThreads < numToScrape:
+                    t1 = threading.Thread(target=getLinks, args=(links[key][i], links))
+                    t2 = threading.Thread(target=getLinks, args=(links[key][i+1], links))
+                    t3 = threading.Thread(target=getLinks, args=(links[key][i+2], links))
+                    t4 = threading.Thread(target=getLinks, args=(links[key][i+3], links))
+                    t1Flag = False
+                    t2Flag = False
+                    t3Flag = False
+                    t4Flag = False
+                    if links[key][i] not in visited:
+                        t1.start()
+                        t1Flag = True
+                        visited.append(links[key][i])
+                        count += 1
+                    if links[key][i+1] not in visited:
+                        t2.start()
+                        t2Flag = True
+                        visited.append(links[key][i+1])
+                        count += 1
+                    if links[key][i+2] not in visited:
+                        t3.start()
+                        t3Flag = True
+                        visited.append(links[key][i+2])
+                        count += 1
+                    if links[key][i+3] not in visited:
+                        t4.start()
+                        t4Flag = True
+                        visited.append(links[key][i+3])
+                        count += 1
+                    if t1Flag:
+                        t1.join()
+                    if t2Flag:
+                        t2.join()
+                    if t3Flag:
+                        t3.join()
+                    if t4Flag:
+                        t4.join()
+                else:
+                    countCopy = count
+                    for k in range(min(len(links[key])-i, numToScrape-countCopy)):
+                        getLinks(links[key][i+k], links)
+                        visited.append(links[key][i+k])
+                        count+=1
                 if(count%10 == 0 and count != 0):
                     print(count, ' wiki pages scraped')
                 #time.sleep(2)
                 if(count>=numToScrape):
                     return count
     return count
+
 if __name__ == '__main__':
     main()
