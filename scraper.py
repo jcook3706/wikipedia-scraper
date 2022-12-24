089983e44348d209d8a3ff94636a05b277f76084
jcook3706 <jcook3706@gmail.com>
2022-12-23 22:25:59 -0500

    first working multiprocessed version

--- scraper.py
+++ scraper.py
@@ -2,29 +2,29 @@ import requests
 import time
 import json
 import os
-import threading
+from multiprocessing import Pool
 from bs4 import BeautifulSoup
 from os.path import exists
 
 lastTime= time.time()
+count = 0
+links = {}
+visited = []
+numProcesses = 8
 
-def getLinks(urlEndpoint, links):
-    global lastTime
-    links[urlEndpoint] = []
-    timeDelta = time.time()-lastTime
-    #print(f'Time spent computing: {timeDelta}')
-    lastTime = time.time()
-    response = requests.get(f'https://en.wikipedia.org{urlEndpoint}')
-    soup = BeautifulSoup(response.text, 'html.parser')
-    timeDelta = time.time()-lastTime
-    lastTime = time.time()
-    # print(f'Time spent waiting for response: {timeDelta}')
-    for link in soup.find_all('a'):
-        linkStr = str(link.get('href'))
-        if(linkStr.find(':') != -1):
-            linkStr = linkStr[:linkStr.find(':')]
-        if(validLink(linkStr) and linkStr not in links[urlEndpoint]):
-            links[urlEndpoint].append(linkStr)
+def getLinks(urlEndpoint):
+    global lastTime, visited
+    thisPageLinks = (urlEndpoint, [])
+    if urlEndpoint not in visited:
+        response = requests.get(f'https://en.wikipedia.org{urlEndpoint}')
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for link in soup.find_all('a'):
+            linkStr = str(link.get('href'))
+            if linkStr.find(':') != -1:
+                linkStr = linkStr[:linkStr.find(':')]
+            if validLink(linkStr) and linkStr not in thisPageLinks[1]:
+                thisPageLinks[1].append(linkStr)
+    return thisPageLinks
 
 def validLink(linkStr):
     if(linkStr[0:5] != '/wiki'):
@@ -36,43 +36,43 @@ def validLink(linkStr):
     return True
 
 def main():
+    global count, links, visited
     startTime = time.time()
-    numToScrape = 10
+    numToScrape = 100
     depthToScrape = 2
-    if not exists('links.txt'):
-        links = {}
-        visited = []
-    else:
+    if exists('links.txt'):
         links = json.load(open('links.txt'))
         visited = list(links.keys())
         os.remove('links.txt')
-    if len(links.keys()) == 0:
-        getLinks('/wiki/Web_scraping', links)
-    # print(links)
-    count = bfsIterate(numToScrape, depthToScrape, links, visited)
-    # print(links.keys())
+    else:
+        initLinks = getLinks('/wiki/Web_scraping')
+        links[initLinks[0]] = initLinks[1]
+    startNum = len(links.keys())
+    bfsIterate(numToScrape, depthToScrape, links, visited, startNum)
     json.dump(links, open('links.txt', 'w'))
-    if(count > 0):
-        print(f'Number of pages scraped this time: {count}')
-        print(f'Average time per page: {(time.time()-startTime)/count}')
+    if(len(links.keys()) > 0):
+        print(f'Number of pages scraped this time: {len(links.keys())-startNum}')
+        print(f'Average time per page: {(time.time()-startTime)/(len(links.keys())-startNum)}')
     else:
         print('No pages scraped')
     print(f'Total number of scraped pages: {len(links.keys())}')
     
-def bfsIterate(numToScrape, depthToScrape, links, visited):
-    count = 0
+def bfsIterate(numToScrape, depthToScrape, links, visited, startNum):
+    global count, numProcesses
+    print()
     for i in range(depthToScrape):
         for key in list(links.keys()):
-            for link in links[key]:
-                if link not in visited:
-                    getLinks(link, links)
-                    visited.append(link)
-                    count+=1
-                if(count%10 == 0 and count != 0):
-                    print(count, ' wiki pages scraped')
-                #time.sleep(2)
-                if(count>=numToScrape):
-                    return count
-    return count
+            with Pool(16) as pool:
+                pageChunk = pool.map(getLinks, links[key])
+                for link in pageChunk:
+                    if link[0] not in links.keys():
+                        links[link[0]] = link[1]
+                    if link[0] not in visited:
+                        visited.append(link[0])
+                if len(links.keys())-startNum>=numToScrape:
+                    pool.close()
+                    pool.join()
+                    return
+    return
 if __name__ == '__main__':
     main()
